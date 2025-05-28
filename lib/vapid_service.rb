ae7d6e598154989ef349a0342628d9048617adfc
Soham Chandrashekhar Kalse <skalse@dons.usfca.edu>
2025-05-28 13:25:06 -0700

    Fixed error to access config directly instead of using vapid_keys

--- lib/vapid_service.rb
+++ lib/vapid_service.rb
@@ -9,7 +9,8 @@ class VapidService
 
   def self.vapid_keys
     config = GlobalConfig.get('VAPID_KEYS')
-    return config['VAPID_KEYS'] if config['VAPID_KEYS'].present?
+    # Fix: Access the config directly, not config['VAPID_KEYS']
+    return config if config.present? && config.is_a?(Hash)
 
     # keys don't exist in the database. so let's generate and save them
     keys = WebPush.generate_key
@@ -17,8 +18,9 @@ class VapidService
     public_key = ENV.fetch('VAPID_PUBLIC_KEY') { keys.public_key }
     private_key = ENV.fetch('VAPID_PRIVATE_KEY') { keys.private_key }
 
-    i = InstallationConfig.where(name: 'VAPID_KEYS').first_or_create(value: { public_key: public_key, private_key: private_key })
-    i.value
+    vapid_data = { 'public_key' => public_key, 'private_key' => private_key }
+    InstallationConfig.where(name: 'VAPID_KEYS').first_or_create(value: vapid_data)
+    vapid_data
   end
 
   private_class_method :vapid_keys
