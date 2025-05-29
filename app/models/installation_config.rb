bd44a7bf78d1b3557a107e54d4e81d79fe633b87
Soham Chandrashekhar Kalse <skalse@dons.usfca.edu>
2025-05-29 14:47:54 -0700

    Updated version in schema file and commented code in installation_config.rb file

--- app/models/installation_config.rb
+++ app/models/installation_config.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: installation_configs
+#
+#  id               :bigint           not null, primary key
+#  locked           :boolean          default(TRUE), not null
+#  name             :string           not null
+#  serialized_value :jsonb            not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_installation_configs_on_name                 (name) UNIQUE
+#  index_installation_configs_on_name_and_created_at  (name,created_at) UNIQUE
+#
 class InstallationConfig < ApplicationRecord
   # Remove the serialize line entirely and handle JSON directly
   # since the column is already JSONB
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_05_14_045638) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_14_045638) do
   # These extensions should be enabled to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
